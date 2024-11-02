a2a1b2fe52d561748aade002b614da449ee1fa57
EnotikEk <sharovmaksim0123@gmail.com>
2024-11-02 10:37:59 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,57 @@
+#class Car:
+#    color = None
+#    fuel = None 
+#    righthand = None
+#    light = None
+
+#    def go(self):
+#        pass
+#    def turn(self):
+#        pass 
+#    def stop(self):
+#        pass
+    
+#myCar = Car()
+
+#myCar.color = 'red'
+#myCar.fuel = 50  
+#myCar.righthand = 'right hand'
+#myCar.light = 'white'
+
+
+#myCar.go()
+#myCar.turn()
+#myCar.stop()
+
+#print(myCar.color)
+#print(myCar.fuel) 
+#print(myCar.righthand) 
+#print(myCar.light)
+
+class moto:
+    color = None
+    wheel = None
+    fuel = None
+
+    def go(self):
+        pass
+    def turn(self):
+        pass 
+    def stop(self):
+        pass
+
+
+myMoto = moto()
+myMoto.color = 'purple'
+myMoto.wheel = 2
+myMoto.fuel = 40
+
+
+myMoto.go()
+myMoto.turn()
+myMoto.stop()
+
+
+print(myMoto.color)
+print(myMoto.wheel)
+print(myMoto.fuel)
